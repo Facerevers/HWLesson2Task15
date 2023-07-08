5528f1ee4d86d02def5c7db450ec52179f105ca4
crazle <crazle@mail.ru>
2023-07-08 14:35:10 +0300

    Является ли день выходным

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите число от 1 до 7 включительно нажмите Enter");
+int number = int.Parse(Console.ReadLine());
+if (number == 6 || number == 7) {Console.WriteLine("Да");}
+else {Console.WriteLine("Нет");}
